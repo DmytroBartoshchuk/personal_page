8f9d44023f13e4f20bbda858dbda58764c5d7f40
Dmytro Bartoshchuk <dmytro.bartoshchuk@hycom.pl>
2018-04-21 12:44:02 +0200

    fix

--- survey/context_processors.py
+++ survey/context_processors.py
@@ -3,10 +3,10 @@ from .models import CustomLink
 
 def add_links_to_context(request):
     links = {
-        'youtube': CustomLink.objects.get(name='youtube').link,
-        'facebook': CustomLink.objects.get(name='facebook').link,
-        'instagram': CustomLink.objects.get(name='instagram').link,
-        'github': CustomLink.objects.get(name='github').link,
+        # 'youtube': CustomLink.objects.get(name='youtube').link,
+        # 'facebook': CustomLink.objects.get(name='facebook').link,
+        # 'instagram': CustomLink.objects.get(name='instagram').link,
+        # 'github': CustomLink.objects.get(name='github').link,
     }
     return {
         'links': links
